0bde8c730fc126583ed4c3e38bbf94e1ebfa2c4c
lucaca-dot <179270779+lucaca-dot@users.noreply.github.com>
2025-03-12 15:22:31 -0700

    I finished it
    I finished it

--- P8SaucedoU1PlayerControl/Assets/Scripts/FollowPlayer.cs
+++ P8SaucedoU1PlayerControl/Assets/Scripts/FollowPlayer.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject Player;
+    public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 backwards = new Vector3(0, 4, 1);
 
 
     // Start is called before the first frame update
@@ -18,6 +19,11 @@ public class FollowPlayer : MonoBehaviour
     void LateUpdate()
     {
         // Offset the camera behind the player by adding to the player's posision
-        transform.position = Player.transform.position + offset;
+        transform.position = player.transform.position + offset;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position = player.transform.position + backwards;
+            transform.rotation = player.transform.rotation;
+        }
     }
 }
