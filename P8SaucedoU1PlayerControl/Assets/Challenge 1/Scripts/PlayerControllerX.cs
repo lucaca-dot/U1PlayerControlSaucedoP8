341039c958c45db9c755fbd1121c807066a8dcb6
lucaca-dot <179270779+lucaca-dot@users.noreply.github.com>
2025-03-14 15:18:42 -0700

    I did challenge 1
    I completed my challenge 1

---
+++ P8SaucedoU1PlayerControl/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float rotationSpeed;
+    public float verticalInput;
+    public float horizontalInput;
+    public float turnspeed = 55.0f;
+    public float plane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.left * Time.deltaTime * turnspeed * verticalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnspeed * horizontalInput);
+
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+    }
+}
--- P8SaucedoU1PlayerControl/Assets/Scripts/PlayerControler.cs
+++ P8SaucedoU1PlayerControl/Assets/Scripts/PlayerControler.cs
@@ -7,7 +7,7 @@ public class PlayerControler : MonoBehaviour
     private float speed = 20;
     private float turnspeed = 45.0f;
     private float horizontalInput;
-    private float forwardInput;
+    private float verticalInput;
 
 
     // Start is called before the first frame update
@@ -19,12 +19,17 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxis("Vertical");
         
-        // Moves the car forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        // Rotates the car based on horizontal input
-        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
+
+        // we will make the plane go up and down
+    
     }
 }
+
+
+
+
